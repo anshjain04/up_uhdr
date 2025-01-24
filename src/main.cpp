e63a7240359bfc9423c871af4eb9809320581809
anshjain04 <ansh.jain@ittiam.com>
2025-01-24 15:21:21 +0530

    init

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include "../build/config.h"
+
+#ifdef LIBUHDR_INCLUDED
+#include "ultrahdr_api.h"
+// #include "libcore.a"
+#endif
+
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+    return 0;
+}
